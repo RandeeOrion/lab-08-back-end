1dab07725bb807e83adf23dab434dc4b2182044f
Randee Orion <randeegood@gmail.com>
2020-01-25 14:18:07 -0800

    basic file set up

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# lab-08-back-end
+# City Explorer Back-End
+
+**Author**: Kai Hansen & Randee Orion
+**Version**: 1.0.2 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+With City Explorer: one can find all sorts of good things about a city they wish to make an inquiry on.
+
+## Getting Started
+One must link to the proper APIs and either use the starter code or cry and drink a lot of alcohol as they create the CSS.
+
+## Architecture
+Languages: HTML, CSS, JS,
+Libraries: JSon, JQuery, Handlebars
+API- DarkSky, Eventful, LocationIQ
+
+## Change Log
+1.0.2 - map and weather are now dynamically generated on demand and are no longer hard coded in 
+
+## Feature Progess
+
+### Feature 1
+Number and name of feature: 1. Repository Set Up
+
+Estimate of time needed to complete: 20 minutes
+
+Start time: 6:30
+
+Finish time: 6:45pm
+
+Actual time needed to complete: 15 minutes
+
+Kai drove while Randee navigated.
+
+### Feature 2
+Number and name of feature: Feature 2: Location
+
+Estimate of time needed to complete: 1 hours
+
+Start time: 6:45pm
+
+Finish time: 9:15pm
+
+Actual time needed to complete: 2.5 hours
+
+Kai drove and Randee navigated. 
+
+### Feature 3
+Number and name of feature: Feature 3: Weather
+
+Estimate of time needed to complete: 1.5 hours
+
+Start time: 6:30pm
+
+Finish time: 7:15pm
+
+Actual time needed to complete: 45 minutes
+
+Randee was the driver and Kai was the navigator 
+
+### Feature 4
+Number and name of feature: Feature 4: Events
+
+Estimate of time needed to complete: 
+
+Start time:
+
+Finish time: 
+
+Actual time needed to complete: 
+
---
+++ server.js
@@ -0,0 +1,146 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const superagent = require('superagent');
+
+const client = new pg.Client(process.env.DATABASE_URL);
+
+const PORT = process.env.PORT;
+const app = express();
+app.use(cors());
+let cache = {};
+
+app.get('/', (request, response) => {
+  response.send('This still works!');
+});
+
+// Location Route
+// app.get('/location', (request, response) => {
+//   try {
+//     const geoData = require('./data/geo.json');
+//     const city = request.query.city;
+//     const locationData = new Location (city, geoData);
+//     response.send(locationData);
+//   }
+//   catch(error) {
+//     errorHandler('So sorry, something went wrong.', request, response);
+//   }
+// });
+app.get('/location', locationHandler);
+app.get('/weather', weatherHandler);
+app.get('/eventful', eventfulHandler)
+
+//Location Handler Function
+function locationHandler(request, response){
+  let city = request.query.city;
+  if(cache.city) {
+    let location = cache.city;
+    response.send(location);
+  } else{
+    try{
+      let city = request.query.city;
+      //console.log('city', request.query, city);
+      let key = process.env.GEOCODE_API_KEY;
+      const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
+      //console.log('hi', url);
+
+      superagent.get(url)
+        .then(data => {
+        //console.log(data.body[0]);
+          const geoData = data.body[0];
+          console.log('city and geoData', city, geoData);
+          const location = new Location(city, geoData);
+          cache.city = location;
+          console.log('location', location);
+          response.send(location);
+        })
+        .catch(() => {
+          errorHandler('location superagent broke', request, response);
+        });
+    }
+    catch(error){
+      errorHandler(error, request, response);
+    }
+  }
+}
+
+// Location Constructor
+function Location(city, geoData){
+  this.search_query = city;
+  this.formatted_query = geoData.display_name;
+  this.latitude = geoData.lat;
+  this.longitude = geoData.lon;
+}
+
+function weatherHandler(request, response) {
+  let latitude = request.query.latitude;
+  let longitude = request.query.longitude;
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${latitude},${longitude}`;
+
+  superagent.get(url)
+    .then(data => {
+      const weatherSummaries = data.body.daily.data.map(day =>{
+        return new Weather(day);
+      });
+      response.status(200).json(weatherSummaries);
+    })
+    .catch(()=>{
+      errorHandler('So sorry, something went wrong. Blame the elves.', request, response);
+    });
+}
+
+function Weather(day){
+  this.forecast = day.summary;
+  this.time = new Date(day.time * 1000).toString().slice(0,15);
+}
+
+
+
+
+
+// Weather Route
+// app.get('/weather', (request, response) => {
+//   try {
+//     const forecast = require('./data/darksky.json');
+//     const weatherData = [];
+//     for (let i = 0; i < forecast.daily.data.length; i++){
+//       let dailyForecast = forecast.daily.data[i];
+//       weatherData.push(new Weather(dailyForecast.summary, dailyForecast.time));
+//     }
+//     response.send(weatherData);
+//   }
+//   catch(error) {
+//     errorHandler('So sorry, something went wrong.', request, response);
+//   }
+// });
+
+// // Weather Constructor
+// function Weather(forecast, time){
+//   this.forecast = forecast;
+//   this.time = new Date(time*1000);
+//   console.log(this.time);
+// }
+
+
+client.connect()
+  .then( ()=>{
+    app.listen(PORT, () => {
+      console.log('Server up on: ', PORT);
+    });
+  })
+  .catch(err => {
+    console.error('pg connect error', err);
+  });
+
+
+
+// Error Handler
+function errorHandler(error, request, response) {
+  response.status(500).send(error);
+}
+
+
+
